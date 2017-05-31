cb2e99152826283d181cee370e03525e7485d80e
Jaswanth Manigundan <jas@domesticcat.com.au>
2017-06-01 09:28:41 +1000

    Removed display text

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def whattoeat(postcode):
     displayTextResult = "Try " + selected.get("name") + "\n You can find the menu here " + selected.get("menu_url")
 
     return {
-        "speech": displayTextResult,
-        "displayText": displayTextResult,
+        #"speech": displayTextResult,
+        #"displayText": displayTextResult,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-jash",
